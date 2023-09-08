07141e792cb2ff2f362768d84d3a419ce79f9d66
Kevin Trabajo <kevin.alvarenga93@gmail.com>
2023-09-08 11:12:36 -0600

    new feature added

--- script.js
+++ script.js
@@ -1,60 +1,78 @@
-const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-const originInput = document.getElementById('input-origin');
-const cipher = document.getElementById('cipher');
-const result = document.getElementById('result');
-const rangeId1 = document.getElementById('range');
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// Función para cifrar un texto usando el cifrado César
+function caesarCipher(text, shift) {
+    // Crear un array con el alfabeto
 
-const shiftMessage = () => {
-    const inputText = originInput.value.toUpperCase();
-    const wordArray = [...inputText];
-    printChar(0, wordArray);
+
+    // Convertir el texto a mayúsculas
+    const uppercaseText = text.toUpperCase();
+
+    let result = "";
+
+    // Obtener los elementos HTML necesarios
+    const resultElement = document.getElementById("result");
+
+    // Iterar a través de cada carácter del texto
+    for (let i = 0; i < uppercaseText.length; i++) {
+        const char = uppercaseText[i];
+
+        // Verificar si el carácter está en el alfabeto
+        const index = alphabet.indexOf(char);
+
+        if (index !== -1) {
+            // Calcular la posición cifrada
+            const newIndex = (index + shift) % alphabet.length;
+
+            // Crear un elemento <span> para cada letra
+            const spanChar = document.createElement("span");
+            resultElement.appendChild(spanChar);
+
+            // Llamar a la función de animación para el <span> creado
+            animateChar(spanChar, alphabet[newIndex]);
+        } else {
+            // Si el carácter no está en el alfabeto, mantenerlo sin cambios
+            result += char;
+        }
+    }
 }
 
-const printChar = (currentLetterIndex, wordArray) => {
-    if (wordArray.length === currentLetterIndex) return;
-    wordArray.shift(); // Eliminar el primer elemento del array
-    const spanChar = document.createElement("span");
-    result.appendChild(spanChar);
-    animateChar(spanChar)
-        .then(() => {
-            const charUnencoded = wordArray[currentLetterIndex];
-            spanChar.innerHTML = alphabet.includes(charUnencoded)
-                ? alphabet[(alphabet.indexOf(charUnencoded) + parseInt(rangeId1.value)) % alphabet.length]
-                : charUnencoded;
-            printChar(currentLetterIndex + 1, wordArray);
-            if (currentLetterIndex === wordArray.length - 1) {
-                sendResult(result.textContent);
-            }
-        });
-}
-
-const animateChar = spanChar => {
+// Función para manejar la animación de cada letra
+function animateChar(spanChar, char) {
     let letterChanges = 0;
     return new Promise(resolve => {
         const interval = setInterval(() => {
             spanChar.innerHTML = alphabet[Math.floor(Math.random() * alphabet.length)];
             letterChanges++;
-            if(letterChanges === 3) {
+            if (letterChanges === 10) {
                 clearInterval(interval);
                 resolve();
             }
-        }, 50);
+        }, 100);
+    }).then(() => {
+        spanChar.innerHTML = char;
     });
 }
 
-const onsubmit = e => {
-    e.preventDefault();
-    result.innerHTML = '';
-    shiftMessage();
+// Función para manejar el cifrado cuando se envía el formulario
+function handleCipherSubmit(event) {
+    event.preventDefault();
+
+    // Obtener los elementos HTML necesarios
+    const inputElement = document.getElementById("input-origin");
+    const resultElement = document.getElementById("result");
+    const rangeElement = document.getElementById("range");
+
+    // Limpiar resultados anteriores
+    resultElement.textContent = "";
+
+    // Obtener el valor del campo de entrada de texto y el valor del desplazamiento
+    const textToCipher = inputElement.value;
+    const shiftValue = parseInt(rangeElement.value);
+
+    // Realizar el cifrado César
+    caesarCipher(textToCipher, shiftValue);
 }
 
-const sendResult = (resultText) => {
-    const resultInput = document.createElement("input");
-    resultInput.setAttribute("type", "hidden");
-    resultInput.setAttribute("name", "result");
-    resultInput.setAttribute("value", resultText);
-    cipher.appendChild(resultInput);
-    cipher.submit();
-}
-
-cipher.addEventListener('submit', onsubmit);
+// Agregar un event listener al formulario para manejar el cifrado cuando se envía
+const cipherForm = document.getElementById("cipher");
+cipherForm.addEventListener("submit", handleCipherSubmit);
